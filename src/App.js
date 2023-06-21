76cdb8c41b3578973ee275a8cb2b9b3aacc4c02e
Ashu <anegi6723@gmail.com>
2023-06-21 09:33:14 +0530

    Created sideways navbar in Header.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route />
       </Routes>
